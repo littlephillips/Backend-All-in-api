1e26a9f611df5490f3eca7e2c69e0f359f3e77c1
Steve <stephenkago10@gmail.com>
2023-01-19 18:26:33 +0300

    Create reviews routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
+  resources :reviews
   
   resources :engagements, only: [:index, :create] 
   resources :bookings, only: [:index, :create ]
   resources :events
   resources :admins, only: [:index, :create, :update, :destroy]
   resources :comments, only: [:index, :create]
-  # resources :reviews, only: [:index, :create]
-
   post 'login', to: 'auth#create'
 end 
 
