8f0459e782725c3ca9368f5fd6e897df841a6907
Steve <stephenkago10@gmail.com>
2023-01-19 18:23:32 +0300

    Dropped reviews and comments migrations,models

--- db/migrate/20230113073634_fix_column_name.rb
+++
@@ -1,6 +0,0 @@
-class FixColumnName < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :reviews, :booking_id, :fullname
-    rename_column :reviews, :event_id, :email
-  end
-end
--- db/migrate/20230113081336_drop_tablename.rb
+++
@@ -1,5 +0,0 @@
-class DropTablename < ActiveRecord::Migration[7.0]
-  def change
-    drop_table :reviews 
-  end
-end
