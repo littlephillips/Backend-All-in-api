522cd08afbc548b846882a0622542ab81e8594ae
Steve <stephenkago10@gmail.com>
2023-01-19 18:26:52 +0300

    Reviews migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_13_082351) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_16_114748) do
   create_table "admins", force: :cascade do |t|
     t.string "username"
     t.string "email"
@@ -25,14 +25,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_082351) do
     t.string "email"
     t.string "eventname"
     t.date "eventdate"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "fullname"
-    t.string "email"
-    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -52,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_13_082351) do
     t.string "image_url"
     t.integer "admin_id"
     t.integer "booking_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "fullname"
+    t.string "email"
+    t.string "comment"
+    t.string "eventname"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
